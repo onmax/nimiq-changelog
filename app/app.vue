c381c64c30845c388cda6e157f246224a8fb54ca
onmax <maximogarciamtnez@gmail.com>
2025-09-12 10:55:54 +0200

    chore: fix CSS

--- app/app.vue
+++ app/app.vue
@@ -85,17 +85,9 @@ useSeoMeta({
 </template>
 
 <style>
-/* Style links within changelog content */
-/* Target links inside the changelog version bodies */
-main :not(h1,h2,h3,h4) a {
-  color: var(--color-blue-500);
-  font-weight: 600;
-}
-
-/* More specific targeting for MDC rendered content */
 [data-mdc] :not(h1,h2,h3,h4) a,
 .prose :not(h1,h2,h3,h4) a {
-  color: var(--color-blue-500);
+  color: var(--colors-blue);
   font-weight: 600;
 }
 </style>
